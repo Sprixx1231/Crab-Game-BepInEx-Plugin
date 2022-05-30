fc4bd5ad39c619f057fba2d70627434042a8814c
A E S T H E T I C <62722161+Sprixx1231@users.noreply.github.com>
2022-05-30 16:12:06 +0200

    initial

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using UnityEngine;
+using System;
+using UnhollowerRuntimeLib;
+using BepInEx.Configuration;
+using CrabGamePlugin.components;
+
+namespace CrabGamePlugin
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Crab Game.exe")]
+    public class Plugin : BasePlugin
+    {
+
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            
+
+            ClassInjector.RegisterTypeInIl2Cpp<Esp>();
+            new GameObject("EPIC HAX")
+            {
+                hideFlags = HideFlags.HideAndDontSave //The GameObject is not shown in the Hierarchy, not saved to to Scenes, and not unloaded by Resources.UnloadUnusedAssets.
+            }.AddComponent<Esp>();
+        }
+    }
+}
---
+++ components/Esp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using HarmonyLib;
+using CrabGamePlugin.util;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace CrabGamePlugin.components;
+
+internal class Esp : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUIStyle guistyle = new GUIStyle();
+        guistyle.normal.textColor = Color.green;
+        GUI.Label(new Rect(10f, 40f, 260f, 20f), "Crab Menu v1.0 [Made by SpreadMouse#1121]", guistyle);
+
+        foreach (var player in FindObjectsOfType<MonoBehaviourPublicCSstReshTrheObplBojuUnique>())
+        {
+            //player positions
+            Vector3 pivPos = player.transform.position;
+            Vector3 playerFootPos;
+            playerFootPos.x = pivPos.x;
+            playerFootPos.y = pivPos.y - 2f;
+            playerFootPos.z = pivPos.z;
+
+            Vector3 playerHeadPos;
+            playerHeadPos.x = pivPos.x;
+            playerHeadPos.y = pivPos.y + 2f;
+            playerHeadPos.z = pivPos.z;
+               
+            //Screen Position
+            var cam = Camera.main;
+            Vector3 wtsFoot = cam!.WorldToScreenPoint(playerFootPos);
+            Vector3 wtsHead = cam!.WorldToScreenPoint(playerHeadPos);
+
+
+            if (wtsFoot.z > 0f)
+            {
+                DrawEsp(wtsFoot, wtsHead,Color.red, 2f );
+            }
+        }
+
+        /*foreach (var player in (Object.FindObjectsOfType(typeof(MonoBehaviourPublicCSstReshTrheObplBojuUnique)) as MonoBehaviourPublicCSstReshTrheObplBojuUnique[])!)
+        {
+            //player positions
+            Vector3 pivPos = player.transform.position;
+            Vector3 playerFootPos;
+            playerFootPos.x = pivPos.x;
+            playerFootPos.y = pivPos.y - 2f;
+            playerFootPos.z = pivPos.z;
+
+            Vector3 playerHeadPos;
+            playerHeadPos.x = pivPos.x;
+            playerHeadPos.y = pivPos.y + 2f;
+            playerHeadPos.z = pivPos.z;
+               
+            //Screen Position
+            var cam = Camera.main;
+            Vector3 wtsFoot = cam!.WorldToScreenPoint(playerFootPos);
+            Vector3 wtsHead = cam!.WorldToScreenPoint(playerHeadPos);
+
+
+            if (wtsFoot.z > 0f)
+            {
+                DrawEsp(wtsFoot, wtsHead,Color.red, 2f );
+            }
+        } */
+        
+    }
+    public void DrawEsp(Vector3 footPos, Vector3 headPos, Color color,  float thiccness = 2f)
+    {
+        float height = headPos.y - footPos.y;
+        float widthOffset = 2;
+        float width = height / widthOffset;
+            
+        //Rendering
+        Render.DrawBox(footPos.x - (width / 2), (float)Screen.height - footPos.y - height, width, height, color,thiccness);
+            
+        //Snap line 
+        Render.DrawLine(new Vector2((float)(Screen.width / 2), (float)(Screen.height / 2)),new Vector2(footPos.x, (float)Screen.height - footPos.y), color, thiccness);
+            
+    }
+}
